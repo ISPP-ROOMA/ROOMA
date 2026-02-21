85894b161f2f5b6f804703ec94aaa9c2648ce6e3
rafaelroodrgz <rafrodmunn@alum.us.es>
2026-02-21 02:32:41 +0100

    feat: add ApartmentDTO and request/response records

---
+++ backend/src/main/java/com/example/demo/Apartment/DTOs/UpdateApartment.java
@@ -0,0 +1,13 @@
+package com.example.demo.Apartment.DTOs;
+
+import jakarta.validation.constraints.NotNull;
+
+public record UpdateApartment(
+        @NotNull String title,
+        @NotNull String description,
+        @NotNull Double price,
+        String bills,
+        @NotNull String ubication,
+        @NotNull String state
+) {
+}
