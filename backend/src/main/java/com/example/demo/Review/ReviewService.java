57302c73f95ad5be51cb839613e412f0a0fb1490
davchacum <davchacum@alum.us.es>
2026-02-28 23:34:28 +0100

    feat: update makeReviewByLandlord method to remove reviewerUserId parameter and enhance review creation logic

--- backend/src/main/java/com/example/demo/Review/ReviewService.java
+++ backend/src/main/java/com/example/demo/Review/ReviewService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.example.demo.Exceptions.ResourceNotFoundException;
 import com.example.demo.MemberApartment.ApartmentMemberEntity;
 import com.example.demo.MemberApartment.ApartmentMemberService;
+import com.example.demo.User.UserEntity;
 import com.example.demo.User.UserService;
 
 import jakarta.transaction.Transactional;
@@ -35,24 +36,31 @@ public class ReviewService {
         return reviewRepositoriy.save(review);
     }
 
-    public Review makeReviewByLandlord(Integer reviewerUserId, Integer reviewedUserId, Integer apartmentId, String content, Integer rating) {
-        
+    public Review makeReviewByLandlord(Integer reviewedUserId, Integer apartmentId, String content, Integer rating) {
+        String email= userService.findCurrentUser();
+        UserEntity reviewerUser = userService.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("Current user not found"));
+
+        Integer reviewerUserId = reviewerUser.getId();
         if(reviewRepositoriy.findReviewsByReviewerUserIdAndReviewedUserIdAndApartmentId(reviewerUserId, reviewedUserId, apartmentId).isPresent()) {
             throw new ResourceNotFoundException("Review already exists for the given reviewer, reviewed user and apartment");
         }
+
         Review review = new Review();
         review.setRating(rating);
         review.setComment(content);
         review.setPublished(false);
         review.setReviewDate(LocalDateTime.now());
+    
         apartmentMemberService.checkUserIsLandlord(apartmentId, reviewerUserId);
         ApartmentMemberEntity reviewMember = apartmentMemberService.findLandlordByApartmentId(apartmentId);
         review.setReviewMember(reviewMember);
+
         ApartmentMemberEntity reviewedMember = apartmentMemberService.findLastMembershipByUserId(reviewedUserId);
         if(reviewedMember.getApartment().getId() != apartmentId) {
              throw new ResourceNotFoundException("Reviewed user is not a member of the apartment"); 
         }
         review.setReviewedMember(reviewedMember);
+        
         return reviewRepositoriy.save(review);
     }
     
