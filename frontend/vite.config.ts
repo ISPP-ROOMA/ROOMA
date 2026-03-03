908591d8c350cd3df49d4c8d8be3d8ebac9a68e2
manueljsanchezg <manueljsanchez10@gmail.com>
2026-03-03 22:41:10 +0100

    feat: Added pwa plugin to install the app in mobile phone

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,9 +12,30 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Rooma',
+        short_name: 'Rooma',
+        description: 'A room booking application',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/rooma_192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/rooma_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ],
+      }
+    })
   ],
   server: {
     host: true,
     port: 5173,
-  },
+  }
 })
