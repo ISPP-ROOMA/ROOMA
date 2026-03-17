be193a5255e8dfe2b5f8f72982055b8151920178
Marco Antonio Herrera Luján <marherluj@alum.us.es>
2026-03-17 20:03:04 +0100

    feat: added functions to get apartments ordered chronologically

--- backend/src/main/java/com/example/demo/Incident/IncidentRepository.java
+++ backend/src/main/java/com/example/demo/Incident/IncidentRepository.java
@@ -3,13 +3,13 @@ package com.example.demo.Incident;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface IncidentRepository extends JpaRepository<IncidentEntity, Integer> {
-    
-    @Query("SELECT i FROM IncidentEntity i WHERE i.apartment.id = :apartmentId")
-    List<IncidentEntity> findByApartmentId(Integer apartmentId);
 
-    
+    List<IncidentEntity> findByApartmentIdOrderByCreatedAtDesc(Integer apartmentId);
+
+    List<IncidentEntity> findByApartmentIdAndStatusInOrderByCreatedAtDesc(Integer apartmentId, List<IncidentStatus> statuses);
+
+    List<IncidentEntity> findByStatusAndUpdatedAtBefore(IncidentStatus status, java.time.LocalDateTime threshold);
 
 }
